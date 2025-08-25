9c667236a7a2197ed50cf986712713e12867cab1
sprite7000 <s45251531@gmail.com>
2025-08-25 11:54:31 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1756111827,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/fishing_net_splash_hit_water_fast_002_29504.m4a",
+		"media/fishing_net_splash_hit_water_fast_002_29504.ogg",
+		"media/9507bd6b0f5d370.m4a",
+		"media/9507bd6b0f5d370.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
